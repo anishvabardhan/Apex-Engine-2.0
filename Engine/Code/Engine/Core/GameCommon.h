4595e28ef0c169866c466ac6b6b365dabdf62a2d
Anishva Bardhan <anishva.bardhan@gmail.com>
2021-11-23 20:08:42 +0530

    See Description- Engine/ProtoGame2D
    Created a GameCommon file and Stored hard -coded values in it

--- Engine/Code/Engine/Core/GameCommon.h
+++ Engine/Code/Engine/Core/GameCommon.h
@@ -2,6 +2,7 @@
 
 #include "CoreIncludes.h"
 
-const Vec4 WINDOW_DIMS = Vec4(0.0f, 1024.0f, 0.0f, 1024.0f);
-const std::string& SHADER_XML = "../Data/Materials/shader.xml";
-const std::string& SCREENSHADER_XML = "../Data/Materials/screenShader.xml";
+const Vec4 APEX_WINDOW_DIMS = Vec4(0.0f, 1024.0f, 0.0f, 1024.0f);
+const std::string& APEX_SHADER_XML = "../Data/Materials/shader.xml";
+const std::string& APEX_SCREENSHADER_XML = "../Data/Materials/screenShader.xml";
+const std::string& APEX_DEFAULT_TEXTURE = "../Data/Textures/Default.png";
--- ProtoGame2D/Code/Game/Framework/Game.cpp
+++ ProtoGame2D/Code/Game/Framework/Game.cpp
@@ -8,10 +8,10 @@
 
 Game::Game()
 {
-	m_ShaderDef = new ShaderDefinition(*ShaderDefinition::InitializeDef(SHADER_XML));
+	m_ShaderDef = new ShaderDefinition(*ShaderDefinition::InitializeDef(APEX_SHADER_XML));
 	m_Shader = new Shader(m_ShaderDef);
 	
-	m_ScreenShaderDef = new ShaderDefinition(*ShaderDefinition::InitializeDef(SCREENSHADER_XML));
+	m_ScreenShaderDef = new ShaderDefinition(*ShaderDefinition::InitializeDef(APEX_SCREENSHADER_XML));
 	m_ScreenShader = Renderer::GetInstance()->GetOrCreateShader(m_ScreenShaderDef);
 	
 	Renderer::GetInstance()->EnableBlend(ParseBlendFac[m_Shader->GetSRC()], ParseBlendFac[m_Shader->GetDST()], ParseBlendOp[m_Shader->GetOP()]);
@@ -50,7 +50,7 @@ void Game::BeginFrame()
 
 	m_Shader->Bind();
 
-	Mat4 camera = Mat4::orthographic(WINDOW_DIMS.m_X, WINDOW_DIMS.m_Y, WINDOW_DIMS.m_Z, WINDOW_DIMS.m_W, -2.0f, 2.0f);
+	Mat4 camera = Mat4::orthographic(APEX_WINDOW_DIMS.m_X, APEX_WINDOW_DIMS.m_Y, APEX_WINDOW_DIMS.m_Z, APEX_WINDOW_DIMS.m_W, -2.0f, 2.0f);
 
 	m_Shader->SetUniformMat4f("u_Proj", camera);
 }
@@ -61,7 +61,7 @@ void Game::Update()
 
 void Game::Render()
 {
-		Renderer::GetInstance()->DrawQuad(Vec2(200.0f, 200.0f), Vec2(100.0f, 50.0f), Vec4(1.0f, 0.0f, 0.0f, 1.0f), "../Data/Textures/stripes.png", *m_Shader);
+		Renderer::GetInstance()->DrawQuad(Vec2(200.0f, 200.0f), Vec2(100.0f, 50.0f), Vec4(1.0f, 0.0f, 0.0f, 1.0f), APEX_DEFAULT_TEXTURE, *m_Shader);
 }
 
 void Game::EndFrame()
@@ -74,7 +74,7 @@ void Game::EndFrame()
 	m_ScreenShader->Bind();
 
 	Renderer::GetInstance()->CopyFrameBuffer(m_CurrentBuffer, m_NextBuffer);
-	Renderer::GetInstance()->DrawFrameBuffer(Vec2(WINDOW_DIMS.m_X, WINDOW_DIMS.m_Z), Vec2(WINDOW_DIMS.m_Y, WINDOW_DIMS.m_W));
+	Renderer::GetInstance()->DrawFrameBuffer(Vec2(APEX_WINDOW_DIMS.m_X, APEX_WINDOW_DIMS.m_Z), Vec2(APEX_WINDOW_DIMS.m_Y, APEX_WINDOW_DIMS.m_W));
 
 	Renderer::GetInstance()->SwappingBuffers();
 }
